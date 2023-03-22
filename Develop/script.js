34d92b8512ce97b6bda0e16cf50d4d7b0e2ecf8c
Russell <rnc720@gmail.com>
2023-03-21 19:38:08 -0500

    Changes before page deployed: - In ReadME, added content under description, under Live URL, under Resouces Used on Project, and under Contributors - In script.js, added comments throughout code explaining what is the intended use of the specific code.

--- Develop/script.js
+++ Develop/script.js
@@ -3,26 +3,33 @@
 // Defining variables for the password criteria
 var lower_Characters = "abcdefghijklmnopqrstuvwxyz";
 var upper_Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var special_Characters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var special_Characters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var numbers = "0123456789";
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Defined password length as undefined to be able to store information in it later.
+var password_length;
+
 function passwordLength() {
 
  // User chooses password character length from between 8 to 128 characters.
-  var password_length = window.prompt ("Choose how many characters would you like this password to be? Please choose from a minimum of 8 to maximum of 128 characters.");
+  password_length = window.prompt ("Choose how many characters would you like this password to be? Please choose from a minimum of 8 to maximum of 128 characters.");
     
    if (password_length >= 8 && password_length <=128) {
     confirmCharacters();
    } else {
        window.alert ("invalid option, please choose between 8 to 128 characters");
        passwordLength();
-   }
 
+   }
+ return password_length;
 };
+
+// storage of confirmed password criteria
+var passwordCriteria = ""
 
 // User chooses the characters they want in their password.
 function confirmCharacters() {
@@ -30,19 +37,43 @@ function confirmCharacters() {
   var yes_lower_characters = confirm ("Do you want to have lowercase characters in your password?");
   var yes_upper_characters = confirm ("Do you want to have uppercase characters in your password?");
   var yes_special_characters = confirm ("Do you want to have special characters in your password?");
-  var yes_numbers = confirm ("Do you want to have numbers in your password?")
+  var yes_numbers = confirm ("Do you want to have numbers in your password?");
    
    if (!yes_lower_characters && !yes_upper_characters && !yes_special_characters && !yes_numbers) {
     window.alert ("invalid entry, please choose to include at least one set of characters in your password");
-    confirmCharacters();
+    return confirmCharacters();
    }
-
-
+   // This section's "if statements" confirms if the user wants the password character included and then pulls the variable information from lines 4 to 7.
+   if (yes_lower_characters) {
+    passwordCriteria += lower_Characters;
+   }
+   if (yes_upper_characters) {
+    passwordCriteria += upper_Characters;
+   }
+   if (yes_special_characters) {
+    passwordCriteria += special_Characters;
+   }
+   if (yes_numbers) {
+    passwordCriteria += numbers;
+   }
+  
+  return passwordCriteria; 
 };
 
+
+
+// combines the confirmCharacters & passwordLength
 function generatePassword() {
- var confirmed_length = passwordLength();
+ passwordLength();
+
+ var storedPassword = ""
+ for (var i = 0; i < password_length; i++) {
+  var generateRandom = Math.floor(Math.random() * passwordCriteria.length);
+  var character = passwordCriteria.charAt(generateRandom);
+  storedPassword += character;
+ }
  
+ return storedPassword;
 }
 
 
@@ -53,6 +84,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  passwordCriteria = ""
 
 }
 
--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Unique-Password-Generator
 
 ## Description
-
-
-## Changes On Code
+- Provided with a starter code to the password generator I started to work on the JavaScript file to fill in the parts to meet the acceptance criteria. This password generator will take in user inputs via window prompts for specifics about the password they would like to generate. It will ask the length of the password from 8 to 128 characters and provide an alert if the input received is outside of the range provided. It will also ask what types of characters the user would like to have included in their password from lowercase and upper case letters, special characters and numbers. It will also provide an alert if the user does choose at least one type of character to include.
 
 
 ## Live URL
 
 
-<!-- <img src="" width=500 height=800 alt=""> -->
+<img src="Develop/assets/images/screenshot.html.png" width=500 height=800 alt="preview of password generator">
 
 ## Resources Used on Project
-1. Password Special Characters
-2. 
+1. Password Special Characters: https://owasp.org/www-community/password-special-characters
+2. Mini Project - Rock, Paper, Scissors
+3. MDN Web Docs - JavaScript
 
 ## Contributors
 1. caballero-r
 2. Xandromus
+3. shanelbb
